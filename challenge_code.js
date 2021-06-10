892eb3954a063495720d3d665100fc8ad8903eac
k-burn <karaburnett93@gmail.com>
2021-06-10 11:00:42 -0500

    remove sort code

--- challenge_code.js
+++ challenge_code.js
@@ -1,32 +1,34 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
-
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            /*
-            when idle, go to bottom floor
-            seems like most people come from bottom floor in my experience
-            */
-            elevator.goToFloor(0);
-        });
         
-        // when floor button pressed, add floor to destination queue
-        elevator.on("floor_button_pressed", function(floorNum) {
-            elevator.goToFloor(floorNum, true);
-            elevator.destinationQueue = elevator.destinationQueue.sort(); 
-            elevator.checkDestinationQueue(); 
-        });
-         
-        // when floor requests an elevator, send elevator to that floor
-        for (var f = 0; f < floors.length; f++) {
-            var floor = floors[f];
-            floor.on("up_button_pressed down_button_pressed", function() {
-                elevator.goToFloor(floor.floorNum());
+        function activateElevator(elevator) {
+            // Whenever the elevator is idle (has no more queued destinations) ...
+            elevator.on("idle", function() {
+                 /*
+                when idle, go to bottom floor
+                seems like most people come from bottom floor in my experience
+                */
+                elevator.goToFloor(0);
             });
+
+            // when floor button pressed, go to requested floor
+            elevator.on("floor_button_pressed", function(floorNum) {
+                elevator.goToFloor(floorNum)
+            });
+            
+            // when floor requests an elevator, send elevator to that floor
+            for (let f = 0; f < floors.length; f++) {
+                let floor = floors[f];
+                floor.on("up_button_pressed down_button_pressed", function() {
+                    elevator.goToFloor(floor.floorNum());
+                });
+            }
         };
+        
+        elevators.forEach(activateElevator);
+
     },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+        update: function(dt, elevators, floors) {
+            // We normally don't need to do anything here
+        }
 }
