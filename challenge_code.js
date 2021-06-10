cf49d573473a723e1fed8f3f96abab6dc42b8ed2
k-burn <karaburnett93@gmail.com>
2021-06-10 11:02:19 -0500

    add passing_floor capabilities

--- challenge_code.js
+++ challenge_code.js
@@ -15,6 +15,29 @@
             elevator.on("floor_button_pressed", function(floorNum) {
                 elevator.goToFloor(floorNum)
             });
+
+            elevator.on("passing_floor", function(floorNum, direction) {
+                // check to see if the floor we're passing has been selected
+                let pressedFloors = elevator.getPressedFloors();
+                if (pressedFloors.includes(floorNum)) {
+                    elevator.goToFloor(floorNum, true);
+                };
+
+                // check to see if there are any passengers waiting on the floor selected
+                // if the direction is the same way we're heading, let them on
+                let floor = floors[floorNum];
+                floor.on("up_button_pressed", function() {
+                    if (elevator.direction == "up") {
+                        elevator.goToFloor(floorNum, true);
+                    };
+                });
+                
+                floor.on("down_button_pressed", function() {
+                    if (elevator.direction == "down") {
+                        elevator.goToFloor(floorNum, true);
+                    };
+                });
+            });
             
             // when floor requests an elevator, send elevator to that floor
             for (let f = 0; f < floors.length; f++) {
