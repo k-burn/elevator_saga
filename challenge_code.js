dd65ff65e46c8ae488fc6843a4b9c888cb393546
k-burn <karaburnett93@gmail.com>
2021-06-10 10:44:09 -0500

    have elevator visit floors requested from waiting passengers

--- challenge_code.js
+++ challenge_code.js
@@ -12,6 +12,18 @@
         elevator.on("floor_button_pressed", function(floorNum) {
             elevator.goToFloor(floorNum)
         });
+        
+        // when floor requests an elevator, send elevator to that floor
+        for (let f = 0; f < floors.length; f++) {
+            let floor = floors[f];
+            floor.on("up_button_pressed", function() {
+                elevator.goToFloor(floor.floorNum());
+            });
+            floor.on("down_button_pressed", function() {
+                elevator.goToFloor(floor.floorNum());
+            });
+        }
+        
     },
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
